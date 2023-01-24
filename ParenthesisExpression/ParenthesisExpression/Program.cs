cfdf18f948f306f6d23e7189d7278e8b08d033a1
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-01-24 15:16:54 +0500

    init

---
+++ ParenthesisExpression/ParenthesisExpression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ParenthesisExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string simbol = "(((())(())))";
+            char leftSimbol = '(';
+            char rightSimbol = ')';
+            int countLeft = 0;
+            int countRight = 0;
+            int maximumDepth = 0;
+
+            foreach (var item in simbol)
+            {
+                if (item == leftSimbol)
+                {
+                    countLeft++;
+                }
+                else if (item == rightSimbol)
+                {
+                    countRight++;
+                }
+            }
+
+            Console.WriteLine();
+
+            countLeft--;
+            countRight--;
+            maximumDepth = countRight;
+
+            if (countLeft != countRight)
+            {
+                Console.WriteLine("строка не корректная");
+            }
+            else if (countLeft == countRight)
+            {
+                Console.WriteLine($"строка корректная и максимум глубины = {maximumDepth}");
+            }
+        }
+    }
+}
